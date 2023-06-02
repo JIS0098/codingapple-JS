f2cb2467efb3a85e7b19b8464fe266017fc25100
Jisoo Han <llqrw55@naver.com>
2023-06-03 00:12:26 +0900

    JS LEVEL2 끝!

--- tab.js
+++ tab.js
@@ -4,13 +4,23 @@
 //모든 쇼 제거
 //버튼 누르면 쇼  추가
 
+
+
+
 var 버튼 = $('.tab-button');
 var 설명 = $('.tab-content');
-for(let i = 0; i<버튼.length; i++){
-    버튼.eq(i).on('click',function(){
-        버튼.removeClass('orange');
-        버튼.eq(i).addClass('orange')
-        설명.removeClass('show');
-        설명.eq(i).addClass('show');
-    })
+
+
+$('.list').on('click',function(e){
+    탭열기(e.target.dataset.id)
+
+})
+
+
+function 탭열기(숫자){
+    버튼.removeClass('orange');
+    버튼.eq(숫자).addClass('orange')
+    설명.removeClass('show');
+    설명.eq(숫자).addClass('show');
 }
+
